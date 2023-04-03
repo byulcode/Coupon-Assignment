ef7462737e6232f964d111b24cd1ff6b4209cc86
byulcode <bybit0408@gmail.com>
2023-04-03 17:40:40 +0900

    refactor : ResponseEntity 사용하도록 수정

--- src/main/java/study/coupon/controller/CouponController.java
+++ src/main/java/study/coupon/controller/CouponController.java
@@ -1,5 +1,8 @@
 package study.coupon.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import study.coupon.domain.dto.RequestDto;
 import study.coupon.domain.dto.ResponseDto;
@@ -8,55 +11,56 @@ import study.coupon.service.CouponService;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/coupon")
 public class CouponController {
 
     private final CouponService couponService;
-    public CouponController(CouponService couponService) {
-        this.couponService = couponService;
-    }
+
     /**
      * 쿠폰 등록
      */
-    @PostMapping("/create")
-    public ResponseDto createCoupon(@RequestBody RequestDto requestDto) {
-        return couponService.saveCoupon(requestDto);
+    @PostMapping
+    public ResponseEntity<?> createCoupon(@RequestBody RequestDto requestDto) {
+        ResponseDto responseDto = couponService.saveCoupon(requestDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
     /**
      * 쿠폰 수정
      */
-    @PatchMapping("/update/{id}")
-    public ResponseDto updateCoupon(@PathVariable Long id, @RequestBody RequestDto requestDto) {
-        return couponService.updateCoupon(id, requestDto);
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateCoupon(@PathVariable Long id, @RequestBody RequestDto requestDto) {
+        ResponseDto responseDto = couponService.updateCoupon(id, requestDto);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     /**
      * 특정 아이디의 쿠폰 조회
      */
     @GetMapping("/{id}")
-    public ResponseDto getCoupon(@PathVariable Long id) {
-        return couponService.getCoupon(id);
+    public ResponseEntity<?> getCoupon(@PathVariable Long id) {
+        ResponseDto responseDto = couponService.getCoupon(id);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     /**
      * 모든 쿠폰 조회
      * codeType=kor 전달시 쿠폰코드에 한글이 포함된 쿠폰만 조회
      */
-    @GetMapping("/list")
-    public List<ResponseDto> allCoupons(@RequestParam(value = "codeType", required = false, defaultValue = "") String codeType) {
-        if (codeType.equals("kor")) {
-            return couponService.findCouponTypeKor(codeType);
-        }
-        return couponService.allCoupons();
+    @GetMapping
+    public ResponseEntity<List<?>> allCoupons(@RequestParam(value = "codeType", required = false, defaultValue = "") String codeType) {
+        List<ResponseDto> list = couponService.allCoupons();
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     /**
      * 쿠폰 삭제
      */
-    @DeleteMapping("/delete/{id}")
-    public void deleteCoupon(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteCoupon(@PathVariable Long id) {
         couponService.deleteCoupon(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
